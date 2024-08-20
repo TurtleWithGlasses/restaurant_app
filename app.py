e4e631c74c4b449e61889c2ca425bd85db5a18e6
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2024-08-20 17:15:59 +0300

    Remove debug lines, change the logic of countdown in html file

--- app.py
+++ app.py
@@ -25,38 +25,30 @@ def index():
 @app.route("/calculate", methods=["POST"])
 def calculate():
     selected_items = {}
-    
+
     food_total = 0
     drink_total = 0
     dessert_total = 0
     
-    # Collect selected items and calculate totals
+    # Collect selected items
     for item, price in food_dict.items():
         quantity = int(request.form.get(f"food_{item}", 0))
-        print(f"Food {item}: {quantity}")  # Debugging line
         if quantity > 0:
             selected_items[item] = quantity
-            food_total += quantity * price
 
     for item, price in drink_dict.items():
         quantity = int(request.form.get(f"drink_{item}", 0))
-        print(f"Drink {item}: {quantity}")  # Debugging line
         if quantity > 0:
             selected_items[item] = quantity
-            drink_total += quantity * price
 
     for item, price in dessert_dict.items():
         quantity = int(request.form.get(f"dessert_{item}", 0))
-        print(f"Dessert {item}: {quantity}")  # Debugging line
         if quantity > 0:
             selected_items[item] = quantity
-            dessert_total += quantity * price
-
-    total = food_total + drink_total + dessert_total
-    vat = total * 0.18
-    total_including_vat = total + vat
     
-    print(f"Total: {total}, VAT: {vat}, Total incl. VAT: {total_including_vat}")  # Debugging line
+    total = food_total + drink_total + dessert_total
+    vat = total *0.18
+    total_including_vat = total + vat
     
     # Send the order to worker.py
     worker_response = send_order_to_worker(selected_items)
@@ -66,11 +58,11 @@ def calculate():
         food_dict=food_dict,
         drink_dict=drink_dict,
         dessert_dict=dessert_dict,
-        food_total=food_total,
+        food_total = food_total,
         drink_total=drink_total,
         dessert_total=dessert_total,
         total_including_vat=total_including_vat,
-        selected_items=selected_items,
+        selected_items = selected_items,
         preparation_details=worker_response.get("preparation_details", {}),
         worker_response=worker_response["status"]
     )
